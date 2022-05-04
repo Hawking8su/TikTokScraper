ac39050826c5cffab4ab3096befc00c766f79e1f
Hawking8su <lzhhawking@126.com>
2022-05-05 00:47:00 +0800

    rename TkUploader.py to Test file

--- TkUploaderTest.py
+++ TkUploaderTest.py
@@ -5,12 +5,17 @@ Background:
 Reference:
 1. https://github.com/redianmarku/tiktok-autouploader/blob/main/run.py
 2. https://github.com/makiisthenes/TiktokAutoUploader
+3. https://piprogramming.org/articles/How-to-make-Selenium-undetectable-and-stealth--7-Ways-to-hide-your-Bot-Automation-from-Detection-0000000017.html
 
 TODO:
 1. Problem: cannot find the right element
 '''
 import time
+
+from selenium.webdriver.common.by import By
+
 from selenium import webdriver
+
 
 
 print('=====================================================================================================')
@@ -41,14 +46,23 @@ driver.set_window_size(1080, 600)
 
 # Send a get request to the url
 driver.get('https://www.tiktok.com/login')
-time.sleep(20)
+time.sleep(10)
 # login manually
 driver.get('https://www.tiktok.com/upload/?lang=en')
-time.sleep(5)
+time.sleep(10)
 
 # Find upload button
-xpath = '/html/body/div[1]/div/div/div/div/div[2]/div[1]/div/div/div[4]/button/div/div'
-elements = driver.find_elements_by_xpath(xpath)  ## empty list, cannot find the right element
+# xpath = '/html/body/div[1]/div/div/div/div/div[2]/div[1]/div/div/div[4]/button/div/div'
+# xpath = '//*[@id="main"]/div[2]/div/div[2]/div[2]/div/div/input'
+# xpath = '/html/body/div[1]/div/div/div/div/div[2]/div[1]/div/div/div[4]/button/div/div'
+xpath = '/html/body/div[1]/div/div/div/div/div[2]/div[1]/div/div/div[4]'
+xpath = '/html/body/div[1]/div/div/div/div/div[2]'
+
+driver.find_element(by=By.XPATH, value=xpath).click() ## empty list, cannot find the right element
+
+
+elements = driver.find_elements(by=By.CLASS_NAME, value='css-ku5jwq')
+print("Element - select file: " + str(elements))
 
 ## todo:
 ## step1: auto login
